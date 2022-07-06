f52a2843a1c2f47c498daeb13086fc7d8fc11f2b
Dao Duong <dao.duong@saritasa.com>
2022-07-06 15:25:41 +0700

    JC19-354 create aired dto

---
+++ libs/core/dtos/aired.dto.ts
@@ -0,0 +1,9 @@
+/** Aired time DTO. */
+export interface AiredDto {
+
+  /** Aired start, for example, "2014-12-20T17:30:50.416Z". */
+  readonly start: Date;
+
+  /** Aired end, for example, "2014-12-20T17:30:50.416Z". */
+  readonly end: Date;
+}
