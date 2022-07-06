19e1484055d8b0c71196b605bf3d706f9c3f18c2
Dao Duong <dao.duong@saritasa.com>
2022-07-06 17:48:58 +0700

    JC19-354 refactor folder

--- apps/vanilla/src/composable/API.ts
+++
@@ -1,5 +0,0 @@
-import axios, { AxiosInstance } from 'axios';
-export const api: AxiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`,
-  headers: { 'Api-Key': `${import.meta.env.VITE_API_KEY}`, 'Accept': 'application/json' },
-});
--- apps/vanilla/src/composable/anime.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './API';
-export const getAnime = async(limit: number, offset: number): Promise<IAnime> => {
-  const { data } = await api.get(`anime/anime/?limit=${limit}&offset=${offset}`, {
-    headers: {
-      Accept: 'application/json',
-    },
-  });
-  return data;
-};
