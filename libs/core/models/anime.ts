35b7e5eba8d40f4d2ce3580c9d249fead8b66acc
Dao Duong <dao.duong@saritasa.com>
2022-07-06 15:29:16 +0700

    JC19-354 remove aired model from anime model

--- libs/core/models/anime.ts
+++ libs/core/models/anime.ts
@@ -1,20 +1,5 @@
 import { OmitImmerable, Immerable } from './immerable';
-
-/** Anime aired time. */
-export class AiredTime extends Immerable {
-
-  /** Anime start date. */
-  public readonly start: Date;
-
-  /** Anime end date. */
-  public readonly end: Date;
-
-  public constructor(data: InitArgsAiredTime) {
-    super();
-    this.end = data.end;
-    this.start = data.start;
-  }
-}
+import { Aired } from './aired';
 
 /** Anime. */
 export class Anime extends Immerable {
@@ -25,13 +10,13 @@ export class Anime extends Immerable {
   public readonly image: string;
 
   /** Title in English. */
-  public readonly titleEnglish: string;
+  public readonly englishTitle: string;
 
   /** Title in Japanese. */
-  public readonly titleJapanese: string;
+  public readonly japaneseTitle: string;
 
   /** Release and end dates. */
-  public readonly aired: AiredTime;
+  public readonly aired: Aired;
 
   /** Anime type. */
   public readonly type: string;
@@ -43,13 +28,12 @@ export class Anime extends Immerable {
     super();
     this.id = data.id;
     this.image = data.image;
-    this.titleEnglish = data.titleEnglish;
-    this.titleJapanese = data.titleJapanese;
+    this.englishTitle = data.englishTitle;
+    this.japaneseTitle = data.japaneseTitle;
     this.aired = data.aired;
     this.type = data.type;
     this.status = data.status;
   }
 }
 
-type InitArgsAiredTime = OmitImmerable<AiredTime>;
 type InitArgsAnime = OmitImmerable<Anime>;
