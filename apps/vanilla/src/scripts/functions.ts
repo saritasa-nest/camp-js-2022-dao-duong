6feff376eddc31c5d3d7230c1902fe1a03711a81
Dao Duong <dao.duong@saritasa.com>
2022-07-07 13:42:05 +0700

    JC19-354 create useful function

---
+++ apps/vanilla/src/scripts/functions.ts
@@ -0,0 +1,31 @@
+
+/** Convert response date object to readable format.
+ * @param date Date data from response object.
+ */
+export const dayConverter = (date: Date | null): number | string => {
+  if (date !== null) {
+    return new Date(date).getFullYear();
+  }
+  return 'None';
+};
+
+/** Checks whether has sort options enabled.
+ * @param option Option value to check.
+ */
+export const hasSortOption = (option: string): boolean => option !== '';
+
+/** Change state of the order direction.
+ * @param hasOption Whether has sort option enabled.
+ */
+export const changeDirectionState = (hasOption: boolean): void => {
+  const sortDirection: HTMLSelectElement = document.querySelector('#sort-direction');
+  if (sortDirection) {
+    if (hasOption) {
+      sortDirection.disabled = false;
+      sortDirection.style.cursor = 'default';
+    } else {
+      sortDirection.disabled = true;
+      sortDirection.style.cursor = 'not-allowed';
+    }
+  }
+};
