661783c82a719d9ca8c6e9fab70a183a00665278
Dao Duong <dao.duong@saritasa.com>
2022-07-07 13:42:25 +0700

    JC19-354 create sort setting interface

---
+++ apps/vanilla/src/scripts/interfaces.ts
@@ -0,0 +1,19 @@
+/** Sort setting. */
+export interface SortSetting {
+
+  /** Option for sort. */
+  option: string;
+
+  /** Direction for sort. */
+  direction: string;
+}
+
+/** Sort options. */
+export interface SortOption {
+
+  /** Sort option text. */
+  text: string;
+
+  /** Sort option value. */
+  value: string;
+}
