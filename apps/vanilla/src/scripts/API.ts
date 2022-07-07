07cd4fc37e6ebdaacc135f9232c518c60adfe434
Dao Duong <dao.duong@saritasa.com>
2022-07-07 15:49:16 +0700

    remove annotate for axios variable #JC19-354

--- apps/vanilla/src/scripts/API.ts
+++ apps/vanilla/src/scripts/API.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
-export const api: AxiosInstance = axios.create({
+import axios from 'axios';
+export const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}`,
   headers: { 'Api-Key': `${import.meta.env.VITE_API_KEY}`, 'Accept': 'application/json' },
 });
