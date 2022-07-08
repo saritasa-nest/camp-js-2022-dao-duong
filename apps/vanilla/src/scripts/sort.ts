47d078e07e56b2b7528242b313868a506bb88fcb
Dao Duong <dao.duong@saritasa.com>
2022-07-08 17:35:03 +0700

    apply variable name changing

--- apps/vanilla/src/scripts/sort.ts
+++ apps/vanilla/src/scripts/sort.ts
@@ -3,42 +3,47 @@ import { Pagination } from '@js-camp/core/models/pagination';
 import { assertNonNullish } from '@js-camp/core/utils/assertNonNullish';
 
 import { renderAnimeTable } from './animeTable';
-import { PAGINATION_STATE, SORT_SETTINGS, SORT_DIRECTION, SORT_OPTIONS, LIMIT, FIRST_PAGE } from './variables';
+import { PAGINATION_STATE, SORT_DIRECTIONS, SORT_OPTIONS, LIMIT, SORT_SETTINGS, FIRST_PAGE } from './variables';
 import { getAnime } from './anime';
-import { changeDirectionState, hasSortOption } from './functions';
-import { PaginationOptions } from './interfaces';
+import { setDirectionState, hasSortOption } from './functions';
+import { PaginationState, SortSetting, PaginationConfig } from './interfaces';
 
 /**  Render sort options.*/
 export const renderSortOptions = (): void => {
-  const sortOptions = document.querySelectorAll('.sort');
-  const sortOption = document.querySelector<HTMLSelectElement>('#sort-option');
-  const sortDirection = document.querySelector<HTMLSelectElement>('#sort-direction');
+  const sortOptions = document.querySelectorAll('.sort__selectElement');
+  const sortOption = document.querySelector<HTMLSelectElement>('.sort__option');
+  const sortDirection = document.querySelector<HTMLSelectElement>('.sort__direction');
+
   assertNonNullish(sortOption);
   assertNonNullish(sortDirection);
     sortOption.innerHTML = ``;
     sortDirection.innerHTML = ``;
-
     SORT_OPTIONS.forEach(option => {
-      sortOption.innerHTML += `<option value="${option.value}" class="sort-option">${option.text}</option>`;
+      sortOption.innerHTML += `<option value="${option.value}" class="select__option">${option.text}</option>`;
     });
-
-    SORT_DIRECTION.forEach(direction => {
-      sortDirection.innerHTML += `<option value="${direction.value}" class="sort-option">${direction.text}</option>`;
+    SORT_DIRECTIONS.forEach(direction => {
+      sortDirection.innerHTML += `<option value="${direction.value}" class="select__option">${direction.text}</option>`;
     });
-    changeDirectionState(hasSortOption(SORT_SETTINGS.option));
+    setDirectionState(hasSortOption(SORT_SETTINGS.option));
     sortOptions.forEach(element => {
       element.addEventListener('change', async(): Promise<void> => {
+        const paginationState: PaginationState = PAGINATION_STATE;
+        const ordering: SortSetting = {
+          ...SORT_SETTINGS,
+          direction: sortDirection.value,
+          option: sortOption.value,
+        };
         PAGINATION_STATE.page = FIRST_PAGE;
-        PAGINATION_STATE.active = FIRST_PAGE;
         SORT_SETTINGS.direction = sortDirection.value;
         SORT_SETTINGS.option = sortOption.value;
-        changeDirectionState(hasSortOption(SORT_SETTINGS.option));
-        const paginationOptions: PaginationOptions = {
+
+        setDirectionState(hasSortOption(ordering.option));
+        const paginationConfig: PaginationConfig = {
           limit: LIMIT,
-          page: PAGINATION_STATE.page,
-          sortSettings: SORT_SETTINGS,
+          page: paginationState.page,
+          ordering,
         };
-        const data: Pagination<Anime> = await getAnime(paginationOptions);
+        const data: Pagination<Anime> = await getAnime(paginationConfig);
         renderAnimeTable(data);
       });
     });
