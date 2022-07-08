9e8888ab6a9c6635c260ee1958baa10fed90e6af
Dao Duong <dao.duong@saritasa.com>
2022-07-08 13:00:01 +0700

    disable eslint warning #JC19-354

--- apps/vanilla/src/scripts/API.ts
+++ apps/vanilla/src/scripts/API.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}`,
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   headers: { 'Api-Key': `${import.meta.env.VITE_API_KEY}`, 'Accept': 'application/json' },
 });
