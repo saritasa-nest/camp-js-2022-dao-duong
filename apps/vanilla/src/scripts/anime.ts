f5a93f315c0fb8c342822af3c2b987744dc3812c
Dao Duong <dao.duong@saritasa.com>
2022-07-08 15:35:50 +0700

    add jsdoc #JC19-354

--- apps/vanilla/src/scripts/anime.ts
+++ apps/vanilla/src/scripts/anime.ts
@@ -5,9 +5,13 @@ import { Pagination } from '@js-camp/core/models/pagination';
 import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
 
 import { api } from './API';
-import { PaginationOptions } from './interfaces';
+import { PaginationConfig } from './interfaces';
 
-export const getAnime = async({ limit, page, sortSettings }: PaginationOptions): Promise<Pagination<Anime>> => {
+/**
+ * Get anime data from the server.
+ * @param PaginationConfig Option for request parameters.
+ */
+export const getAnime = async({ limit, page, sortSettings }: PaginationConfig): Promise<Pagination<Anime>> => {
   const offset: number = limit * (page - 1);
   const { data } = await api.get<PaginationDto<AnimeDto>>(
     `anime/anime/?limit=${limit}&offset=${offset}&ordering=${sortSettings.direction}${sortSettings.option}`,
