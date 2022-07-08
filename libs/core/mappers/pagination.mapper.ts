f1e0c48b147e0445556bb9b283afa294bbae2404
Dao Duong <dao.duong@saritasa.com>
2022-07-08 15:57:48 +0700

    fix pagination mapper flexibility #JC19-354

--- libs/core/mappers/pagination.mapper.ts
+++ libs/core/mappers/pagination.mapper.ts
@@ -1,21 +1,18 @@
 import { PaginationDto } from '../dtos/pagination.dto';
 import { Pagination } from '../models/pagination';
-import { AnimeDto } from '../dtos/anime.dto';
-import { Anime } from '../models/anime';
-
-import { AnimeMapper } from './anime.mapper';
 export namespace PaginationMapper {
 
   /**
    * Maps dto to model.
    * @param dto Pagination dto.
+   * @param mapperFromDto Callback of result mapper from DTO to Model.
    */
-  export function fromDto(dto: PaginationDto<AnimeDto>): Pagination<Anime> {
-    return new Pagination<Anime>({
+  export function fromDto<Dto, Model>(dto: PaginationDto<Dto>, mapperFromDto: (resultDto: Dto) => Model): Pagination<Model> {
+    return new Pagination<Model>({
       count: dto.count,
       next: dto.next,
       previous: dto.previous,
-      results: dto.results.map(result => AnimeMapper.fromDto(result)),
+      results: dto.results.map(result => mapperFromDto(result)),
     });
   }
 }
