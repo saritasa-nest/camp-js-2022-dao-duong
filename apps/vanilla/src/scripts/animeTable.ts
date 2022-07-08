0a45c1bbd5a035480697137c736919ac16d3a82b
Dao Duong <dao.duong@saritasa.com>
2022-07-08 14:37:07 +0700

    use reduct for table body #JC19-354

--- apps/vanilla/src/scripts/animeTable.ts
+++ apps/vanilla/src/scripts/animeTable.ts
@@ -2,7 +2,7 @@ import { Anime } from '@js-camp/core/models/anime';
 import { Pagination } from '@js-camp/core/models/pagination';
 import { assertNonNullish } from '@js-camp/core/utils/assertNonNullish';
 
-import { dayConverter } from '../scripts/functions';
+import { dateConverter } from '../scripts/functions';
 
 import { renderPaginateButton } from './pagination';
 import { LIMIT } from './variables';
@@ -14,21 +14,18 @@ import { LIMIT } from './variables';
 export const renderAnimeTable = (dataset: Pagination<Anime>): void => {
   const dataTable = document.querySelector<HTMLDivElement>('.anime-table');
   assertNonNullish(dataTable);
-  let tableBody = ``;
-  dataset.results.forEach(anime => {
-    tableBody += `
-      <tr>
-        <td>
-          <img class="anime-image" src="${anime.image}" alt="Anime image">
-        </td>
-        <td>${anime.englishTitle}</td>
-        <td>${anime.japaneseTitle}</td>
-        <td>${dayConverter(anime.aired.start)}</td>
-        <td>${anime.type}</td>
-        <td>${anime.status}</td>
-      </tr>
-    `;
-  });
+  const tableBody = dataset.results.reduce((previousValue: string, currentValue: Anime): string => `${previousValue}
+    <tr>
+         <td>
+           <img class="anime-image" src="${currentValue.image}" alt="Anime image">
+         </td>
+         <td>${currentValue.englishTitle}</td>
+         <td>${currentValue.japaneseTitle}</td>
+         <td>${dateConverter(currentValue.aired.start)}</td>
+         <td>${currentValue.type}</td>
+         <td>${currentValue.status}</td>
+       </tr>
+    `, '');
   dataTable.innerHTML = `
       <table class="responsive-table centered highlight" >
         <thead>
