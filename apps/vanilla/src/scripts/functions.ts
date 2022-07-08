9fd11554113889f94b5cf779700c57e0063ee878
Dao Duong <dao.duong@saritasa.com>
2022-07-08 10:39:24 +0700

    format jsdoc string #JC19-354

--- apps/vanilla/src/scripts/functions.ts
+++ apps/vanilla/src/scripts/functions.ts
@@ -1,8 +1,10 @@
+import { assertNonNullish } from '@js-camp/core/utils/assertNonNullish';
 
-/** Convert response date object to readable format.
+/**
+ * Convert response date object to readable format.
  * @param date Date data from response object.
  */
-export const dayConverter = (date: Date | null): string => {
+export const dayConverter = (date: string | null): string => {
   if (date !== null) {
     return (new Date(date).getFullYear())
       .toString();
@@ -10,23 +12,24 @@ export const dayConverter = (date: Date | null): string => {
   return 'None';
 };
 
-/** Checks whether has sort options enabled.
+/**
+ * Checks whether has sort options enabled.
  * @param option Option value to check.
  */
 export const hasSortOption = (option: string): boolean => option !== '';
 
-/** Change state of the order direction.
+/**
+ * Change state of the order direction.
  * @param hasOption Whether has sort option enabled.
  */
 export const changeDirectionState = (hasOption: boolean): void => {
-  const sortDirection: HTMLSelectElement = document.querySelector('#sort-direction');
-  if (sortDirection) {
+  const sortDirection = document.querySelector<HTMLSelectElement>('#sort-direction');
+  assertNonNullish(sortDirection);
     if (hasOption) {
       sortDirection.disabled = false;
       sortDirection.style.cursor = 'default';
     } else {
       sortDirection.disabled = true;
       sortDirection.style.cursor = 'not-allowed';
-    }
   }
 };
