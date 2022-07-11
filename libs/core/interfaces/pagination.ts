f0ab0bbcd48c355f68e9a890199b0837ce266d60
Dao Duong <dao.duong@saritasa.com>
2022-07-11 15:45:37 +0700

    rewrite doc for ordering #JC19-354

--- libs/core/interfaces/pagination.ts
+++ libs/core/interfaces/pagination.ts
@@ -4,9 +4,9 @@ export interface PaginationConfig {
   /** Number of items per request. */
   readonly limit: number;
 
-  /**  */
+  /**  Current page base on total items and limit. */
   readonly page: number;
 
-  /** Setting for sort features. */
+  /** Sort order params for request query string. */
   readonly ordering: string;
 }
