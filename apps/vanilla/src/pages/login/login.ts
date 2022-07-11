0d514a14c0a4a86f88a677b719f21c1fdebb2ebe
Dao Duong <dao.duong@saritasa.com>
2022-07-11 14:07:02 +0700

    create login page #JC19-357

---
+++ apps/vanilla/src/pages/login/login.ts
@@ -0,0 +1,32 @@
+// import { Register } from '@js-camp/core/models/register';
+import { Login } from '@js-camp/core/models/login';
+import { assertNonNull } from '@js-camp/core/utils/assertNonNull';
+
+import { Url } from '../../scripts/constants';
+
+import { isAuthenticated } from '../../scripts/functions';
+import { Helpers } from '../../scripts/helpers';
+import { navigate } from '../../scripts/navigation';
+import { AuthService } from '../../services/authService';
+
+const loginForm = document.querySelector<HTMLFormElement>('.form');
+
+window.addEventListener('load', () => {
+  Helpers.clearToken();
+  console.log(isAuthenticated());
+});
+
+assertNonNull(loginForm);
+loginForm.addEventListener('submit', async(event): Promise<void> => {
+  event.preventDefault();
+  const emailTextInput = document.querySelector<HTMLInputElement>('input[name="email"]');
+  assertNonNull(emailTextInput);
+  const passwordTextInput = document.querySelector<HTMLInputElement>('input[name="password"]');
+  assertNonNull(passwordTextInput);
+  const loginData: Login = {
+    email: emailTextInput.value,
+    password: passwordTextInput.value,
+  };
+  await AuthService.login(loginData);
+  navigate(Url.Base);
+});
