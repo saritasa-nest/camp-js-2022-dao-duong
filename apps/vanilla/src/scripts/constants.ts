780bbe316f80311fe55426999d6a2265b8152523
Dao Duong <dao.duong@saritasa.com>
2022-07-11 14:10:01 +0700

    create constants #JC19-357

---
+++ apps/vanilla/src/scripts/constants.ts
@@ -0,0 +1,12 @@
+/** Type of token. */
+export enum Token{
+  Refresh = 'REFRESH_TOKEN',
+  Access = 'ACCESS_TOKEN',
+}
+
+/** Available url. */
+export enum Url{
+  Base = '/',
+  Login = '/login/',
+  Register = '/register/',
+}
