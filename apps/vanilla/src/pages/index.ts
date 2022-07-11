31331de3f1d185036e9023b8839d3a2f47a53f1c
Dao Duong <dao.duong@saritasa.com>
2022-07-11 17:52:01 +0700

    add get User #JC19-357

--- apps/vanilla/src/pages/index.ts
+++ apps/vanilla/src/pages/index.ts
@@ -5,11 +5,13 @@ import { isAuthenticated } from '../scripts/functions';
 
 import { Helpers } from '../scripts/helpers';
 import { navigate } from '../scripts/navigation';
+import { AuthService } from '../services/authService';
 
 window.addEventListener('load', () => {
   if (!isAuthenticated()) {
     navigate(Url.Login);
   }
+  AuthService.getUser();
 });
 
 const logoutButton = document.querySelector('.logout-btn');
