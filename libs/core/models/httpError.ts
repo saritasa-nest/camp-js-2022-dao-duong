0d3981e139c6f66f13510729f27c2570e413d1b0
Dao Duong <dao.duong@saritasa.com>
2022-07-12 15:41:02 +0700

    create error model

---
+++ libs/core/models/httpError.ts
@@ -0,0 +1,25 @@
+import { ErrorDataDto } from '../dtos/httpError.dto';
+
+import { Immerable, OmitImmerable } from './immerable';
+
+/** HttpError. */
+export class HttpError extends Immerable {
+
+  /** Id. */
+  public readonly detail: string;
+
+  /** Name. */
+  public readonly code?: string;
+
+  /** Name. */
+  public readonly data?: ErrorDataDto;
+
+  public constructor(error: ErrorInitArgs) {
+    super();
+    this.detail = error.detail;
+    this.data = error.data;
+    this.code = error.code;
+  }
+}
+
+type ErrorInitArgs = OmitImmerable<HttpError>;
