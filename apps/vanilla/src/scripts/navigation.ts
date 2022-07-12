e768c6044fac5b0849c918f3f85553faf68918d2
Dao Duong <dao.duong@saritasa.com>
2022-07-12 15:38:02 +0700

    change navigate function location #JC19-357

--- apps/vanilla/src/scripts/navigation.ts
+++
@@ -1,9 +0,0 @@
-import { Url } from './constants';
-
-/**
- *Navigation functions.
- *@param url Url to navigate to.
- */
-export function navigate(url: Url): void {
-  window.location.href = url;
-}
