e9671becd2eefb9fdd7210dc4071d455e2591724
Dao Duong <dao.duong@saritasa.com>
2022-07-12 10:38:03 +0700

    add render logout function #JC19-357

--- apps/vanilla/src/pages/index.ts
+++ apps/vanilla/src/pages/index.ts
@@ -1,22 +1,11 @@
-import { assertNonNull } from '@js-camp/core/utils/assertNonNull';
-
 import { Url } from '../scripts/constants';
-import { isAuthenticated } from '../scripts/functions';
+import { isAuthenticated, renderLogoutButton } from '../scripts/functions';
 
-import { Helpers } from '../scripts/helpers';
 import { navigate } from '../scripts/navigation';
-import { AuthService } from '../services/authService';
 
-window.addEventListener('load', () => {
+window.addEventListener('load', (): void => {
   if (!isAuthenticated()) {
     navigate(Url.Login);
   }
-  AuthService.getUser();
-});
-
-const logoutButton = document.querySelector('.logout-btn');
-assertNonNull(logoutButton);
-logoutButton.addEventListener('click', () => {
-  Helpers.clearToken();
-  navigate(Url.Login);
+  renderLogoutButton();
 });
--- apps/vanilla/src/scripts/functions.ts
+++ apps/vanilla/src/scripts/functions.ts
@@ -1,8 +1,28 @@
+import { assertNonNull } from '@js-camp/core/utils/assertNonNull';
+
 import { StorageService } from '../services/storageService';
 
-import { Token } from './constants';
+import { Token, Url } from './constants';
+import { Helpers } from './helpers';
+import { navigate } from './navigation';
 
 /** */
 export function isAuthenticated(): boolean {
   return StorageService.get(Token.Access) !== null;
+}
+
+/** Render logout button. */
+export function renderLogoutButton(): void {
+  const { body } = document;
+  body.innerHTML += `
+    <nav class="navbar">
+      <button class="logout-btn btn_right">Logout</button>
+    </nav>
+  `;
+  const logoutButton = document.querySelector('.logout-btn');
+  assertNonNull(logoutButton);
+  logoutButton.addEventListener('click', () => {
+    Helpers.clearToken();
+    navigate(Url.Login);
+});
 }
