27917dcc3966b89baee1ff59b783a6db1a326333
Dao Duong <dao.duong@saritasa.com>
2022-07-13 11:16:10 +0700

    change error data dto interface #JC19-357

--- libs/core/dtos/httpError.dto.ts
+++ libs/core/dtos/httpError.dto.ts
@@ -1,11 +1,5 @@
 export interface ErrorDataDto {
-  readonly email?: readonly string[];
-  readonly first_name?: readonly string[];
-  readonly last_name?: readonly string[];
-  readonly avatar?: readonly string[];
-  readonly password?: readonly string[];
-  readonly token?: readonly string[];
-  readonly non_field_errors?: readonly string[];
+  readonly [key: string]: readonly string[];
 }
 
 export interface HttpErrorDto {
