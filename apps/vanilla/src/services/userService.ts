bfcb017ae04c999cd5d49ee67b9533f5efba2573
Dao Duong <dao.duong@saritasa.com>
2022-07-18 15:41:57 +0700

    add interceptor #JC19-359

--- apps/vanilla/src/services/userService.ts
+++ apps/vanilla/src/services/userService.ts
@@ -2,16 +2,13 @@ import { UserMapper } from '@js-camp/core/mappers/user/user.mapper';
 import { User } from '@js-camp/core/models/user/user';
 
 import { api } from '../api/api';
-import { Token } from '../scripts/constants';
-
-import { StorageService } from './storageService';
 
 export namespace UserService {
 
   /** Get user.*/
   export async function getUser(): Promise<User> {
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    const { data } = await api.get('/users/profile/', { headers: { Authorization: `Bearer ${StorageService.get(Token.Access)}` } });
+    const { data } = await api.get('/users/profile/');
     return UserMapper.fromDto(data);
   }
 }
