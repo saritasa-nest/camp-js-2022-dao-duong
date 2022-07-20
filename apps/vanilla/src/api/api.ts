9dfe8de8bee207251e0040fc3adb9d2ccad30ffd
Dao Duong <dao.duong@saritasa.com>
2022-07-20 12:07:55 +0700

    remove eslint #JC19-359

--- apps/vanilla/src/api/api.ts
+++ apps/vanilla/src/api/api.ts
@@ -4,7 +4,6 @@ import { requestInterceptor } from './interceptor';
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   headers: { 'Api-Key': import.meta.env.VITE_API_KEY, 'Accept': 'application/json' },
 });
 
