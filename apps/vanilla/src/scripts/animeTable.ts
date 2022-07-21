d0f6adb3fb58a8272dc4348ee68fb59c86ee364e
Dao Duong <dao.duong@saritasa.com>
2022-07-21 15:26:48 +0700

    fix eslint #JC19-359

--- apps/vanilla/src/scripts/animeTable.ts
+++ apps/vanilla/src/scripts/animeTable.ts
@@ -1,6 +1,7 @@
 import { Anime } from '@js-camp/core/models/anime/anime';
 import { Pagination } from '@js-camp/core/models/pagination';
 import { assertNonNull } from '@js-camp/core/utils/assertNonNull';
+
 import { StorageService } from '../services/storageService';
 
 import { convertDate } from '../utils/convertDate';
