0a01e697f825a6d075bc7c98033663708b568d7e
Dao Duong <dao.duong@saritasa.com>
2022-08-04 15:01:57 +0700

    megre auth service with user service #JC19-434

--- apps/angular/src/core/services/user.service.ts
+++ apps/angular/src/core/services/user.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
+import { TokenDto } from '@js-camp/core/dtos/auth/token.dto';
 import { Login } from '@js-camp/core/models/auth/login';
 import { Register } from '@js-camp/core/models/auth/register';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 import { ApiService } from './api.service';
-import { AuthService } from './auth.service';
+import { JwtService } from './jwt.service';
 
 /** Api service. */
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  private readonly loginPath = 'auth/login/';
+
+  private readonly registerPath = 'auth/register/';
+
+  private readonly refreshPath = 'auth/refresh/';
+
   public constructor(
     private readonly apiService: ApiService,
-    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
   ) {}
 
   /**
@@ -21,7 +28,10 @@ export class UserService {
    * @param credentials Login credentials.
    */
   public login(credentials: Login): Observable<void> {
-    return this.authService.login(credentials);
+    return this.apiService.post<TokenDto>(this.loginPath, credentials).pipe(map((response => {
+      this.jwtService.saveToken(response);
+      console.log(response);
+    })));
   }
 
   /**
@@ -29,6 +39,14 @@ export class UserService {
    * @param credentials Register credentials.
    */
   public register(credentials: Register): Observable<void> {
-    return this.authService.register(credentials);
+    return this.apiService.post<TokenDto>(this.registerPath, credentials).pipe(map((response => {
+      this.jwtService.saveToken(response);
+      console.log(response);
+    })));
+  }
+
+  /** Login. */
+  public logout(): void {
+    this.jwtService.destroyToken();
   }
 }
