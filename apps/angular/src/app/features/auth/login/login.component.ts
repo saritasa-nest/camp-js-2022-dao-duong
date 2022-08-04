7121cfc3d3dbfa46b9dc09a437dd77b091426b0f
Dao Duong <dao.duong@saritasa.com>
2022-08-04 17:06:20 +0700

    add basic flow to register #JC19-434

--- apps/angular/src/app/features/auth/login/login.component.ts
+++ apps/angular/src/app/features/auth/login/login.component.ts
@@ -1,34 +1,38 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Login } from '@js-camp/core/models/auth/login';
 
 import { UserService } from '../../../../core/services/';
 
+/** Login component. */
 @Component({
   selector: 'camp-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   public constructor(
     private readonly userService: UserService,
     private readonly fb: FormBuilder,
   ) {}
 
+  /** Login form controls. */
   public loginForm = this.fb.group({
     email: ['', Validators.required, Validators.email],
     password: ['', Validators.required],
   });
 
-  public ngOnInit(): void {}
-
+  /** Handle form submission. */
   public submitForm(): void {
-    this.userService
-      .login(this.loginForm.value as Login)
-      .subscribe();
+    if (!this.loginForm.errors) {
+      this.userService
+        .login(this.loginForm.value as Login)
+        .subscribe();
+    }
   }
 
+  /** Handle logout. */
   public logout(): void {
     this.userService.logout();
   }
--- apps/angular/src/app/features/auth/register/register.component.ts
+++ apps/angular/src/app/features/auth/register/register.component.ts
@@ -1,13 +1,44 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Register } from '@js-camp/core/models/auth/register';
 
+import { UserService } from '../../../../core/services/';
+
+/** Register component. */
 @Component({
   selector: 'camp-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterComponent implements OnInit {
-  constructor() {}
+export class RegisterComponent {
+  public constructor(
+    private readonly userService: UserService,
+    private readonly fb: FormBuilder,
+  ) {}
 
-  ngOnInit(): void {}
+  /** Register form controls. */
+  public readonly registerForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    firstName: [''],
+    lastName: [''],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  });
+
+  /** Handle form submission. */
+  public submitForm(): void {
+    this.registerForm.markAllAsTouched();
+    if (this.registerForm.invalid) {
+      return;
+    }
+    this.userService
+      .register(this.registerForm.value as Register)
+      .subscribe();
+  }
+
+  /** Handle logout. */
+  public logout(): void {
+    this.userService.logout();
+  }
 }
