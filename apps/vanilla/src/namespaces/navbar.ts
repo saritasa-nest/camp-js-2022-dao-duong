9e81e132cdf23737ed28f632dd716d37fbaf324a
Dao Duong <dao.duong@saritasa.com>
2022-08-05 13:32:09 +0700

    remove export from renderLogoutButton #JC19-357

--- apps/vanilla/src/namespaces/navbar.ts
+++ apps/vanilla/src/namespaces/navbar.ts
@@ -21,7 +21,7 @@ export namespace Navbar {
   }
 
   /** Render logout button. */
-  export function renderLogoutButton(): void {
+  function renderLogoutButton(): void {
     const navbar = document.querySelector('.navbar');
     assertNonNull(navbar);
     navbar.innerHTML += `<button type="button" class="logout-btn btn-right">Logout</button>`;
