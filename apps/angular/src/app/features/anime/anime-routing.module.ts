fafe6b6b0480b393d56b9a54524ea8f2a4ea3583
DaoDuong <dao-duong@saritasa.com>
2022-08-07 21:19:05 +0700

    remove auth guard from homepage #JC19-434

--- apps/angular/src/app/features/anime/anime-routing.module.ts
+++ apps/angular/src/app/features/anime/anime-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthGuard } from '../../../core/guards/auth.guard';
-
 import { AnimeComponent } from './anime.component';
 
-const routes: Routes = [{ path: '', component: AnimeComponent, canActivate: [AuthGuard] }];
+const routes: Routes = [{ path: '', component: AnimeComponent }];
 
 /** Anime table view routing module. */
 @NgModule({
