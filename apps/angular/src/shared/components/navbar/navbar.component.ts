00560dc38fbc0fe243d8d02a5f60a81ac9763c1b
Dao Duong <dao.duong@saritasa.com>
2022-08-09 12:08:22 +0700

    create navbar component #JC19-434

---
+++ apps/angular/src/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+
+import { Subject, Observable, tap } from 'rxjs';
+
+import { AuthService, NavigateService } from '../../../core/services';
+
+/** Login component. */
+@Component({
+  selector: 'camp-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent implements OnDestroy {
+  private readonly subscriptionDestroyed$: Subject<boolean> = new Subject();
+
+  /** Check whether user authenticated or not. */
+  public isAuthenticated$: Observable<boolean>;
+
+  public constructor(private authService: AuthService, private navigateService: NavigateService) {
+    this.isAuthenticated$ = this.authService.checkAuthentication();
+  }
+
+  /** Handle logout. */
+  public logout(): void {
+    this.authService.logout().pipe(
+      tap(() => this.navigateService.navigateToLogin()),
+    )
+      .subscribe();
+  }
+
+  /** @inheritdoc */
+  public ngOnDestroy(): void {
+    this.subscriptionDestroyed$.next(true);
+    this.subscriptionDestroyed$.complete();
+  }
+}
--- apps/angular/src/shared/shared.module.ts
+++ apps/angular/src/shared/shared.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 /** Shared module. */
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [NavbarComponent],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
+  exports: [NavbarComponent],
 })
 export class SharedModule { }
