2b7bdb430a5b52ffca5201fec75fc869caac21fe
Dao Duong <dao.duong@saritasa.com>
2022-08-15 12:15:00 +0700

    add 404 page #JC19-402

--- apps/angular/src/shared/shared.module.ts
+++ apps/angular/src/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 /** Shared module. */
 @NgModule({
-  declarations: [SpinnerComponent],
+  declarations: [SpinnerComponent, PageNotFoundComponent],
   imports: [CommonModule, MatProgressSpinnerModule],
-  exports: [SpinnerComponent],
+  exports: [SpinnerComponent, PageNotFoundComponent],
 })
 export class SharedModule { }
