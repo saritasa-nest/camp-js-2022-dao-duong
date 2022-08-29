fbafbe8d9eaefb3e656e8347008d96c4ca00381a
DaoDuong <dao-duong@saritasa.com>
2022-08-29 11:16:58 +0700

    add control state #JC19-482

--- apps/react/src/features/anime/components/AnimeListControls/AnimeListControl.tsx
+++ apps/react/src/features/anime/components/AnimeListControls/AnimeListControl.tsx
@@ -1,4 +1,4 @@
-import { AnimeSort, AnimeSortDirection, AnimeSortField } from '@js-camp/core/models/anime/anime';
+import { AnimeSort, AnimeSortDirection, AnimeSortField, AnimeType } from '@js-camp/core/models/anime/anime';
 import { Box, Tabs, Tab } from '@mui/material';
 import { FC, memo, SyntheticEvent, useState } from 'react';
 
@@ -31,28 +31,33 @@ const TabPanel = (props: TabPanelProps) => {
 };
 
 const AnimeListControlComponent: FC = () => {
-  const [value, setValue] = useState(0);
-  const [sortValue, setSortValue] = useState({ direction: AnimeSortDirection.Descending, field: AnimeSortField.EnglishTitle } as AnimeSort);
-  const handleChange = (event: SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+  const [tabValue, setTabValue] = useState(0);
+  const [sortValue, setSortValue] = useState<AnimeSort>({
+    direction: AnimeSortDirection.Descending,
+    field: AnimeSortField.EnglishTitle,
+  });
+  const [filterValue, setFilterValue] = useState<AnimeType | []>([]);
+  const [searchValue, setSearchValue] = useState<string>('');
+  const handleTabChange = (event: SyntheticEvent, newTabValue: number) => {
+    setTabValue(tabValue);
   };
 
   return (
     <Box className={styles['anime-list-control']}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="anime control tabs" sx={{ width: '100%' }}>
+        <Tabs value={tabValue} onChange={handleTabChange} aria-label="anime control tabs" sx={{ width: '100%' }}>
           <Tab label="Sort" sx={{ flex: '1' }}/>
           <Tab label="Type" sx={{ flex: '1' }}/>
           <Tab label="Search" sx={{ flex: '1' }}/>
         </Tabs>
       </Box>
-      <TabPanel value={value} index={0}>
+      <TabPanel value={tabValue} index={0}>
         <Sort sortValue={sortValue} setSortValue={setSortValue}/>
       </TabPanel>
-      <TabPanel value={value} index={1}>
+      <TabPanel value={tabValue} index={1}>
         Item Two
       </TabPanel>
-      <TabPanel value={value} index={2}>
+      <TabPanel value={tabValue} index={2}>
         Item Three
       </TabPanel>
     </Box>
