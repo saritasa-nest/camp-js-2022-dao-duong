cdb253020c0deae67ce1e1252e4ac049181e47c7
Dao Duong <dao.duong@saritasa.com>
2022-09-12 14:03:08 +0700

    add filter options #JC19-502

--- apps/react/src/features/anime/components/AnimeForm/components/FormAutocomplete/FormAutocomplete.tsx
+++ apps/react/src/features/anime/components/AnimeForm/components/FormAutocomplete/FormAutocomplete.tsx
@@ -1,7 +1,8 @@
+import { Genre } from '@js-camp/core/models/anime';
 import { AutocompleteRenderInputParams, TextField } from '@mui/material';
 import { Field } from 'formik';
 import { Autocomplete } from 'formik-mui';
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useState } from 'react';
 
 interface Props<T> {
 
@@ -28,17 +29,42 @@ export const FormAutocomplete = <T extends object>({
   getOptionLabel,
   onChange,
 }: Props<T>) => {
+  const [searchValue, setSearchValue] = useState<string>('');
+
   const handleAutoCompleteChange = (event: SyntheticEvent, value: T[]) => {
+    console.log(value);
     onChange(value);
+  };
+
+  const onInputChange = (event: SyntheticEvent, value: string) => {
+    setSearchValue(value);
+    console.log(value);
+  };
+
+  const filterOptions = (optionss: Genre[]) => {
+    let isMatch = false;
+    const filtered = optionss.filter(option => {
+      if (option.name === searchValue) {
+        isMatch = true;
+      }
+      return option.name.toLowerCase().includes(searchValue.toLowerCase());
+    });
+    if (isMatch) {
+      return filtered;
+    }
+    return [{ name: `Create ${searchValue}` }, ...filtered];
   };
 
   return (
     <Field
       component={Autocomplete}
       multiple
+      freeSolo
       name={name}
       label={label}
+      filterOptions={filterOptions}
       options={options}
+      onInputChange={onInputChange}
       onChange={handleAutoCompleteChange}
       getOptionLabel={(option: T) => getOptionLabel(option)}
       isOptionEqualToValue={(option: T, value: T) => getOptionLabel(option) === getOptionLabel(value)}
